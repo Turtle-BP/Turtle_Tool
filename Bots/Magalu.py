93ca8dfba6814204826addbc3bc8b812a7bd472a
Pedro - Turtle BP <brandprotection.03@turtlebp.com>
2022-07-29 10:19:09 -0300

    Magalu

--- Bots/Magalu.py
+++ Bots/Magalu.py
@@ -66,7 +66,7 @@ def getting_n_creating_magazine_urls(brand,teste_var=None):
     df['Urls'] = df['Brand'] + "+" + df['Name']
 
     # Criando a nova coluna que são as urls de pesquisa
-    df['Urls_search'] = "https://www.magazineluiza.com.br/_next/data/pCll3kSqiWeBgIB0YC-gn/busca/" + \
+    df['Urls_search'] = "https://www.magazineluiza.com.br/_next/data/ZIpIaOx0q3rMCfEgx3tvz/busca/" + \
     df['Brand'][0] + "%2B" + df['Name'] + ".json?slug=busca&slug=" + \
     df['Brand'][0] + "%2B" + df['Name']
 
